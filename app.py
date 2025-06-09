a1dc62d5792d1bdce25c4ee36ea874c591d96b87
shifu <gabrielcastillejo25@gmail.com>
2025-06-08 22:21:55 -0500

    feat: add configuration file and update script to verify if file exists

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
-print('Versión 0')
+import json
+
+
+def main():
+    with open("config.json") as f:
+        config = json.load(f)
+        print(config)
+
+
+if __name__ == "__main__":
+    print('Versión 0')
+    main()
