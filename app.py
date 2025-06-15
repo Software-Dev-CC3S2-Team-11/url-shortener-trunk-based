a274d3d1304859d17a51051c98c9ce90a17046db
Amir Canto <146664121+amirmiir@users.noreply.github.com>
2025-06-14 20:06:48 -0500

    feat: add GET /{slug} endpoint to redirect to original url
    * chore: update issue templates
    
    * feat: create gh actions to prevent pushes on main
    
    feat: add .github/workflow/no-direct-push.yml
    
    * chore: testing the gh actions: no-direct-push.yml
    
    * chore: second test for gh action no-direct-push.yml
    
    * chore: add test-file for testing pushes
    
    * feat: add configuration file and update script to verify if file exists
    
    * chore: add new dependencies in requirements.txt
    
    * feat: add GET /{slug} endpoint to redirect using url_mapping as database
    
    ---------
    
    Co-authored-by: Gabriel Castillejo <gabrielcastillejo25@gmail.com>

--- app.py
+++ app.py
@@ -1,7 +1,33 @@
 import json
+import uvicorn
+from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
+
+app = FastAPI()
+
+# urls acortadas para simular una base de datos
+url_mapping = {
+    'asd': 'https://www.google.com/search?q=asd&ie=UTF-8',
+    'mnp': 'https://www.youtube.com/'
+}
 
 
-def main():
+@app.get('/{slug}')
+def redirect_url(slug: str):
+    """
+    Busca en la base de datos la url
+    asociada al slug(url_acortada)
+    y la redirecciona a su url original
+    """
+
+    # esto simula la busqueda del slug en la base de datos
+    if slug in url_mapping.keys():
+        # redirecciona a la url original
+        return RedirectResponse(url_mapping[slug])
+    return {"error": "url not found"}
+
+
+def read_config():
     with open("config.json") as f:
         config = json.load(f)
         print(config)
@@ -9,4 +35,5 @@ def main():
 
 if __name__ == "__main__":
     print('Versión 0')
-    main()
+    read_config()
+    uvicorn.run("app:app", host="127.0.0.1", port=8000, reload=True)
