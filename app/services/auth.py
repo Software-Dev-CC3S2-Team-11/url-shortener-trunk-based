193011080f595d4f9a471372b8803bc1b688bf71
Alkerg <albert.arg500@gmail.com>
2025-06-17 23:57:36 -0500

    feat: add autentication endpoints in routes and auth service

---
+++ app/services/auth.py
@@ -0,0 +1,36 @@
+from fastapi import APIRouter, HTTPException, Depends
+from fastapi.security import OAuth2PasswordBearer
+from passlib.context import CryptContext
+from pydantic import BaseModel
+from jose import jwt, JWTError
+from datetime import datetime, timedelta
+from models.User import User
+from dotenv import load_dotenv
+from os import getenv
+
+load_dotenv()
+
+SECRET_KEY = getenv('SECRET_KEY')
+ALGORITHM = getenv('ALGORITHM')
+
+bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+def create_access_token(email: str, username: str):
+    """
+    Crea un token JWT con el email y el nombre de usuario del usuario
+    """
+    encode = {'email': email, 'username': username}
+    expires = datetime.utcnow() + timedelta(days=30)
+    encode.update({'exp': expires})
+    return jwt.encode(encode, SECRET_KEY, algorithm=ALGORITHM)
+
+
+def verify_token(token: str):
+    """
+    Verifica el token JWT y devuelve el payload si es válido
+    """
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        return payload
+    except JWTError:
+        return None
