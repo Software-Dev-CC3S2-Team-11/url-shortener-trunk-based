e6dd042ad5bc30b8e9ff427aff05e421edc51159
Alkerg <albert.arg500@gmail.com>
2025-06-17 23:56:10 -0500

    feat: add database connection script

---
+++ app/database/db.py
@@ -0,0 +1,22 @@
+from dotenv import load_dotenv
+from os import getenv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+load_dotenv()
+
+URL = getenv('URL_DB')
+print(URL)
+engine = create_engine(URL)
+SessionLocal = sessionmaker(autoflush=False, bind=engine)
+Base = declarative_base()
+
+def get_db():
+    """
+    Crea una sesión de base de datos y la devuelve.
+    """
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
