1c8c10e0fe98c9541939d2a294ef6aaeba9f457c
amirmiir <00aamircg@gmail.com>
2025-06-18 22:13:13 -0500

    chore: delete print URL

--- app/database/db.py
+++ app/database/db.py
@@ -6,10 +6,11 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 load_dotenv()
 
 URL = getenv('URL_DB')
-print(URL)
+
 engine = create_engine(URL)
 SessionLocal = sessionmaker(autoflush=False, bind=engine)
 Base = declarative_base()
+
 
 def get_db():
     """
